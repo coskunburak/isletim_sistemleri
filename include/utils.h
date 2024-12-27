6b474aea3eba4688bd9a1b333129747bb94cb7ed
Burak Coşkun <111977249+coskunburak@users.noreply.github.com>
2024-12-27 19:01:23 +0300

    utils.h olusturuldu

---
+++ include/utils.h
@@ -0,0 +1,20 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+
+// Gerekli kütüphaneler ve makrolar
+
+#define MAX_CMD_LEN 1024   // Tek satırda girilebilecek maksimum karakter
+#define MAX_ARGS    64     // Parçalanabilecek maksimum argüman sayısı
+#define DELIM       " \t\r\n\a" // Argüman ayracı boşluk/tap/yeni satır
+
+#endif
